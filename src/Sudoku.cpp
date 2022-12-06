65d0c22d44ee167a073f9af8fedc12b0030e9211
cblberlin <bailin.cai@hotmail.fr>
2022-12-06 16:34:20 +0100

    modif coloration

--- src/Sudoku.cpp
+++ src/Sudoku.cpp
@@ -1,10 +1,19 @@
 #include "Sudoku.hpp"
 /*
-Sudoku::Sudoku(vector<vector<int> > Grid){
+Sudoku::Sudoku(vector<vector<int> > Grid) : Graphe_colore(Grid.size()*Grid.size(), Grid.size()){
     assert(Grid.size() == Grid[0].size());
+    //using Graphe_colore::Graphe_colore;
     grid = Grid;
     int n = Grid.size();
-    nb_node = n;
+    for(int i = 0; i < n*n; i++){
+        // ajoute les arêtes pour les lignes
+        for(int j = 0; j < n*n - 1; j++){
+            for(int k = j+1; k < n*n; k++){
+                ajout_arete()
+            }
+        }
+    }
+
 }
 
 
@@ -18,5 +27,4 @@ void Sudoku::afficher_grid(){
         cout << endl;
     }
 }
-
 */

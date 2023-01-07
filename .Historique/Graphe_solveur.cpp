3f7d7ac216c848d2c6d34101a2d2bf5c4af9dfe4
Kakashi1615 <kakashi1615@gmail.com>
2023-01-07 01:09:12 +0100

    historique de la progression du projet

--- .Historique/Graphe_solveur.cpp
+++ .Historique/Graphe_solveur.cpp
@@ -1,21 +1,41 @@
 #include "Graphe_solveur.hpp"
 
 bool Graphe_solveur::est_valide(int u, int couleur) {
-    list<int>::iterator it;
+    vector<int>::iterator it;
     for(it=adj[u].begin();it!=adj[u].end();++it){
     if(color[*it] == couleur) return false;
     }
     return true;
 }
 
-void Graphe_solveur::solve(int sommet) {
-    // Initialisation du tableau des voisins.
-    voisins = &s_Voisins;
-    /* étapes algorithmiques : 
-    1) choisir le sommet ayant le moins de possibilités de couleurs (celui qui a le plus de voisins
-    en cas d'égalité et choix arbitraire si encore égalité);
-    2) Donne la "plus petite couleur" possible à ce sommet, mettre à jour le tableau des couleurs
-    disponibles.
-    3) Appelle la fonction avec sommet pas encore parcouru (voisin en priorité), utiliser
-    "est_valide".
+vector<int> Graphe_solveur::tableau_nombre_couleurs_dispos() {
+    int n = s_Voisins.size();
+    int resultat_auxiliaire = tableau_nombre_voisins();
+    vector<int> resultat [n];
+    for(int i; i < n; i++) {
+        int compteur = 0;
+        m = resultat_auxiliaire[i];
+        for(int j; j < m; j++) {
+            if(color[s_Voisins[j]] != -1) compteur += 1;
+        }
+        resultat[i] = max_color - compteur;
+        // On choisit de ne pas écrire un assert vérifiant qu'il y a au moins une couleur disponible.
+        // On laisse ce détail pour le backtracking.
+    }
+    return resultat;
 }
+
+bool Graphe_solveur::solve(vector<int> & vois, vector<int> & Zahl_Farben) {
+    // Farbe veut dire couleur en allemand, on a déjà utilisé les termes en français et en anglais. Zahl veut dire nombre en allemand.
+    // C'est simplement pour une question de visibilité.
+    // vois correspondra au tableau retourné par tableau_nombre_voisins().
+    // Farbe correspondra au tableau retourné par tableau_nombre_couleurs_dispos().
+    int min_couleur = min(Zahl_Farbe.begin(), Zahl_Farbe.end(), [] (int i) {return color[i] == -1});
+    // On prendra en compte le critère supplémenrtaire "a le plus de voisins dans le cas ou plusieurs sommet ont cette valeur minimum" dès que possible.
+    // En attendant, le sommet sera le plus petit en terme de notation à minimiser le nombre de couleurs disponibles.
+    int sommet_cible = distance(Zahl_Farbe.begin(), min_couleur);
+    for(int i = 0; i < max_color; i++) {
+        
+        color[sommet_cible] ==
+    }
+    }

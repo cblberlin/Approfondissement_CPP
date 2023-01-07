0b9b94ac2699d3ed4658131c9c907b4c69a70560
Kakashi1615 <kakashi1615@gmail.com>
2023-01-07 23:44:58 +0100

    historique de l'avancement

--- .Historique/Graphe_solveur.cpp
+++ .Historique/Graphe_solveur.cpp
@@ -1,8 +1,8 @@
 #include "Graphe_solveur.hpp"
 
 bool Graphe_solveur::est_valide(int noeud, int couleur) {
-    for(int i = 0; i < get_voisins(noeud).size(); i++) {
-        if(G.color[i] == couleur) {
+    for(int i = 0; i < G.voisins(noeud).size(); i++) {
+        if(G.get_color(i) == couleur) {
             return false;
         }
     }
@@ -15,16 +15,16 @@ bool Graphe_solveur::est_valide(int noeud, int couleur) {
 }
 
 vector<int> Graphe_solveur::tableau_nombre_couleurs_dispos() {
-    int n = s_Voisins.size();
-    vector<int> resultat_auxiliaire = tableau_nombre_voisins();
-    vector<int> resultat [n];
-    for(int i; i < n; i++) {
+    int n = G.get_nb_node();
+    vector<int> resultat_auxiliaire = G.tableau_nombre_voisins();
+    vector<int> resultat;
+    for(int i = 0; i < n; i++) {
         int compteur = 0;
-        m = resultat_auxiliaire[i];
-        for(int j; j < m; j++) {
-            if(G.color[G.get_s_Voisins[j]] != -1) compteur += 1;
+        //int m = resultat_auxiliaire[i];
+        for(auto &j: G.voisins(i)) {
+            if(G.get_color(j) != -1) compteur += 1;
         }
-        resultat[i] = max_color - compteur;
+        resultat.push_back(G.get_max_color() - compteur);
         // On choisit de ne pas écrire un assert vérifiant qu'il y a au moins une couleur disponible.
         // On laisse ce détail pour le backtracking.
     }
@@ -44,15 +44,12 @@ bool Graphe_solveur::solve_backtracking(int sommet) { // (int sommet, int couleu
     
     vector<int>::iterator it;
     for(it = tab_couleur_possible.begin(); it != tab_couleur_possible.end(); it++){
-        if(est_valide(sommet, it)){
-            G.colorer(sommet, it);
+        if(est_valide(sommet, *it)){
+            G.colorer(sommet, *it);
             if(solve_backtracking(sommet + 1)){
                 return true;
-    
-            else{
-                G.colorer(sommet, -1);
             }
-            }
+            G.colorer(sommet, -1);
         }
 
     }
@@ -60,7 +57,6 @@ bool Graphe_solveur::solve_backtracking(int sommet) { // (int sommet, int couleu
 }
 
 /*    vector<int> vois_bis = vois;
-
     auto non_colore = [] (int i) {return color[i] == -1;};
     auto valeur = find_if(nbre_couleurs_dispos.begin(), nbre_couleurs_dispos.end(), non_colore);
     if(valeur == nbre_couleurs_dispos.end()) {return true;}
---
+++ .Historique/Sudoku.cpp
@@ -0,0 +1,18 @@
+#include "Sudoku.hpp"
+/*
+Sudoku::Sudoku(vector<vector<int> > Grid){
+    assert(Grid.size() == Grid[0].size());
+    grid = Grid;
+    int n = Grid.size();
+    nb_node = n;
+}
+void Sudoku::afficher_grid(){
+    cout << "le grid de sudoku est:\n";
+    for(int i = 0; i < nb_node; i++){
+        for(int j = 0; j < nb_node; j++){
+            cout << grid[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+*/
